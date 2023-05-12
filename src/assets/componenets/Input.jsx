edc27274b97b97bda569036afcbff81853fc625f
Vedant_Tarale <vedanttarale.211ec257@nitk.edu.in>
2023-05-13 03:18:35 +0530

    Added input for search

--- src/assets/componenets/Input.jsx
+++ src/assets/componenets/Input.jsx
@@ -1,16 +1,30 @@
-const query = "Paris";
-
-
+import { useState } from "react";
+import styles from "../../styles";
 const Input = () => {
+    const [location, setLocation] = useState("");
+    const [data, setData] = useState("");
     const getWeather = () => {
-        fetch("https://api.weatherapi.com/v1/forecast.json?key=cd8187028e6f488182a52355231105&q=" + query + "&days=1&aqi=yes&alerts=yes")
+        fetch("https://api.weatherapi.com/v1/forecast.json?key=cd8187028e6f488182a52355231105&q=" + location + "&days=1&aqi=yes&alerts=yes")
             .then((response) => response.json())
-            .then((json) => console.log(json))
-
+            .then((json) => setData(json))
     }
     return (
         <>
-            <button className="px-3 py-2 m-2 bg-transparent border-2 border-secondary rounded-md text-2xl text-offWhite hover:border-slate-700 transition-all hover:translate-x-0.5 hover:-translate-y-0.5 delay-100 ease-in-out" onClick={getWeather}>Get Forcast</button>
+            <form>
+                <input
+                    type="text"
+                    value={name}
+                    className={styles.textbox}
+                    placeholder="Enter the Location..."
+                    onChange={(e) => setLocation(e.target.value)}
+                />
+            </form>
+            <button className={styles.btn} onClick={getWeather}>Get Forecast</button>
+            {
+                data != "" && (
+                    <p className="text-offWhite"> {JSON.stringify(data)} </p>
+                )
+            }
         </>
 
     )
