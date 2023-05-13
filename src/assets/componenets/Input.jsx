d8366767193a26291e71543abfc76c40ab5f91c5
Vedant_Tarale <vedanttarale.211ec257@nitk.edu.in>
2023-05-14 00:14:02 +0530

    Added lottie file and updated layout

--- src/assets/componenets/Input.jsx
+++ src/assets/componenets/Input.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import styles from "../../styles";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import weather from "../lotties/weather.json"
 const Input = () => {
     const [location, setLocation] = useState("");
     const [data, setData] = useState("");
@@ -10,16 +12,35 @@ const Input = () => {
     }
     return (
         <>
-            <form>
-                <input
-                    type="text"
-                    value={location}
-                    className={styles.textbox}
-                    placeholder="Enter the Location..."
-                    onChange={(e) => setLocation(e.target.value)}
-                />
-            </form>
-            <button className={styles.btn} onClick={getWeather}>Get Forecast</button>
+            <div className="flex flex-wrap justify-center">
+                <div className="basis-full flex items-center justify-center md:basis-7/12">
+                    <form>
+                        <input
+                            type="text"
+                            value={location}
+                            className={styles.textbox}
+                            placeholder="Enter the Location..."
+                            onChange={(e) => setLocation(e.target.value)}
+                        />
+                    </form>
+                    <button className={styles.btn} onClick={getWeather}>Get Forecast</button>
+                </div>
+                <div className="basis-full md:basis-5/12">
+                    <div className="flex justify-center">
+                        <Player
+                            autoplay
+                            loop
+                            src={weather}
+                            style={{ height: "512px"}}
+                        >
+                            <Controls
+                                visible={false}
+                                buttons={["play", "repeat", "frame", "debug"]}
+                            />
+                        </Player>
+                    </div>
+                </div>
+            </div>
             {
                 data != "" && (
                     <p className="text-offWhite"> {JSON.stringify(data)} </p>
