2dbb9caab21c9f99586b40762a82d86e7ddbd8c6
Vedant Tarale <vedanttarale10@gmail.com>
2023-05-25 00:52:29 +0530

    minor UI changes

--- src/assets/componenets/charts/Linechart.jsx
+++ src/assets/componenets/charts/Linechart.jsx
@@ -1,21 +1,21 @@
-import { Line } from 'react-chartjs-2'
-import { Chart as ChartJS } from 'chart.js/auto'
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
 function Linechart({ chartdata }) {
+  const options = {
+    responsive: true,
+    scales: {
+      y: {
+        min: -20,
+        max: 50,
+        stepSize: 20,
+      },
+      x: {},
+    },
+  };
   return (
     <div>
-      <Line 
-        data={chartdata}
-        
-
-        
-        options={{
-            maintainAspectRatio: false,
-        }}
-      
-      
-      />
+      <Line data={chartdata} options={options} />
     </div>
-  )
+  );
 }
-
-export default Linechart
+export default Linechart;
